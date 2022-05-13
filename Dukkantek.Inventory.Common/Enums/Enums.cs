1fb5b0d70dd139ef375b42ef2223bfdde732f491
Umar, Ismail Abiodun <ismail@ismailumar.com>
2022-05-13 10:11:44 +0400

    Add project files.

---
+++ Dukkantek.Inventory.Common/Enums/Enums.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace Dukkantek.Inventory.Common.Enums
+{
+    public enum InvoiceTypeEnum
+    {
+        [Description("General")]
+        General = 0,
+        [Description("Project")]
+        Project = 1,
+        [Description("Service")]
+        Service = 2,
+    }
+
+    public enum DurationTypeEnum
+    {
+        [Description("Weekly")]
+        Weekly = 1,
+        [Description("Monthly")]
+        Monthly = 2,
+        [Description("Quarterly")]
+        Quarterly = 3,
+        [Description("BiAnnually")]
+        BiAnnually = 4,
+        [Description("Annually")]
+        Annually = 5,
+        [Description("Biennially")]
+        Biennially = 6,
+        [Description("Quinquennially")]
+        Quinquennially = 7,
+    }
+}
---
+++ Dukkantek.Inventory.Common/Extensions/DukkantekExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Dukkantek.Inventory.Common.Extensions
+{
+    public static class CinnsolExtensions
+    {
+        public static string Slugify(this string str)
+        {
+            return str.ToLower().Replace(" ", "-").Replace(".", "").Replace("/", "-").Replace("\\", "-").Replace("@", "-").Replace("!", "").Replace(",", "").Replace("~", "").Replace("`", "").Replace("'", "").Replace("\"", "").Replace("#", "").Replace("&", "") + Guid.NewGuid().ToString().Substring(0, 12);
+        }
+
+        public static string GenerateRef(this string str)
+        {
+            return $"{str}{Guid.NewGuid().ToString().Replace("-", "").ToUpper().Substring(0, 10)}";
+        }
+
+        public static string GenerateReferralCode(this string str)
+        {
+            return $"{str}{Guid.NewGuid().ToString().Replace("-", "").ToUpper().Substring(0, 7)}";
+        }
+
+        public static string ToYesNo(this bool value)
+        {
+            return value ? "Yes" : "No";
+        }
+
+        public static string GetDescription(this Enum value)
+        {
+            FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
+            if (fieldInfo == null)
+            {
+                return null;
+            }
+            var attribute = (DescriptionAttribute)fieldInfo.GetCustomAttribute(typeof(DescriptionAttribute));
+            return attribute.Description;
+        }
+
+        public static int ToAge(this DateTime date)
+        {
+            // Save today's date.
+            var today = DateTime.Today;
+
+            // Calculate the age.
+            var age = today.Year - date.Year;
+
+            // Go back to the year in which the person was born in case of a leap year
+            if (date.Date > today.AddYears(-age)) age--;
+
+            return age;
+        }
+    }
+}
